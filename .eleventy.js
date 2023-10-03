e445d775b22dd17ba5aa784917214c12a8b6f3f9
Antonia <familleravel@hotmail.fr>
2023-10-03 13:32:39 +0200

    create files njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+/* Config file */
 module.exports = function(eleventyConfig) {
+  // Ce dossier est cloné lors du build
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/css");
+
   return{
     dir: {
       input: "src",
